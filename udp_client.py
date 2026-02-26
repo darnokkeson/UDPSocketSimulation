369a8007db203d0e9addf4e5cd88b8b4f6ea92be
noise <kooonrad.noise@gmail.com>
2026-02-26 16:29:10 +0100

    change client UDP

--- udp_client.py
+++ udp_client.py
@@ -1,12 +1,12 @@
 from socket import *
 
-serverName = 'localhost'
+serverName = '172.20.10.5'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
 message = input('Input lowercase sentence:')
 
-clientSocket.sendto(message.encode(), (serverName, serverPort))
+clientSocket.sendto('answer from phone server', (serverName, serverPort))
 modifiedMessage, serverAdress = clientSocket.recvfrom(2048)
 
 print(modifiedMessage.decode())
